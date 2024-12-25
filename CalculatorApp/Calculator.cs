9334294b55389a80d681915713fea63febf0e469
mothana404 <mothanaalmajali91@gmail.com>
2024-12-25 19:32:20 +0300

    first project in C#

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    internal class Calculator
+    {
+        private int x = 0 , y = 0;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int Add()
+        {
+            return x + y;
+        }
+        public int Subtract()
+        {
+            return x - y;
+        }
+        public int Multiply()
+        {
+            return x * y;
+        }
+        public string Divide()
+        {
+            if (y == 0)
+            {
+                return "Cannot divide by zero";
+            }
+            return (x / y).ToString();
+        }
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            Console.Write("Enter The Value of X: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Enter The Value of Y: ");
+            y = int.Parse(Console.ReadLine());
+            Calculator Calc = new Calculator();
+            Calc.X = x;
+            Calc.Y = y;
+            Console.WriteLine("Sum: " + Calc.Add());
+            Console.WriteLine("Difference: " + Calc.Subtract());
+            Console.WriteLine("Product: " + Calc.Multiply());
+            Console.WriteLine("Quotient: " + Calc.Divide());
+        }
+    }
+}
